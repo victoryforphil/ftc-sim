cf91a6636b9c15434391a8ca8e32ae66aaa43ee5
Alex Carter <accounts@atecstudios.com>
2017-09-07 21:33:59 -0700

    Ultrasonic Code

---
+++ Assets/Scripts/UltraSonicSensor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltraSonicSensor : MonoBehaviour {
+    public float Distance;
+    public bool debugLines;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FireCase();
+
+    }
+
+    void FireCase()
+    {
+        RaycastHit hit;
+
+        Physics.Raycast(transform.position, transform.forward, out hit);
+
+        Distance = hit.distance;
+
+        if (debugLines)
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+        }
+    }
+}
